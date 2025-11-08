8af4477984446a19364791efe2418defa11a0948
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-11-08 23:17:04 +0300

    تعديل نماذج الحسابات

--- awalimna/users/models.py
+++ awalimna/users/models.py
@@ -51,31 +51,115 @@ class author(models.Model):
 
 # admin_reader model
 class admin_reader(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # admin_author model
 class admin_author(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # super_admin_reader model
 class super_admin_reader(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # super_admin_author model
 class super_admin_author(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # educational_center model
 class educational_center(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # educational_center_teacher model
 class educational_center_teacher(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # educational_center_student model
 class educational_center_student(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=25)
+    address = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    level = models.CharField(max_length=10)
+    country = models.CharField(max_length=25)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 # owner model
 class owner(models.Model):
