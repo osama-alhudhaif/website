2060099468c9620d2bfa6350be3b0dde5b08e4f9
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-11-10 12:11:06 +0300

    تصميم نظام القصص

--- awalimna/stories/models.py
+++ awalimna/stories/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -8,3 +9,9 @@ from django.db import models
 2- Chapter (الفصل)
 3- Genre (التصنيف/النوع)
 """
+
+# تصميم شكل القصة
+class Story(models.Model):
+    id = models.AutoField(primary_key=True) #int only
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
