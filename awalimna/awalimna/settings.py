96bc4394a60e8807bfba29b367a63f327f8608a3
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-11-10 12:11:06 +0300

    تصميم نظام القصص

--- awalimna/awalimna/settings.py
+++ awalimna/awalimna/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'awalimna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data_awalimna',
+        'USER': 'osama-alhudhaif',
+        'PASSWORD': 'Osama-alhudhaif2@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
