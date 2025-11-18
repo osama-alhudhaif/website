00944e2cad60c618b1c98ff54fc38a1227b597de
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-11-18 23:25:58 +0300

    حذف بعض الاشياء

--- awalimna/users/views.py
+++ awalimna/users/views.py
@@ -1,18 +1,4 @@
 from django.shortcuts import render
 import datetime
 
-def profile_view(request):
-    # 1. هذا هو "السياق" (قاموس بايثون)
-    context = {
-        'username': 'أحمد علي',                  # مثال لنص (String)
-        'age': 28,                               # مثال لرقم (Integer)
-        'skills': ['بايثون', 'جانغو', 'CSS'],    # مثال لقائمة (List)
-        'profile_details': {                     # مثال لقاموس (Dictionary)
-            'job': 'مطور ويب',
-            'country': 'السعودية'
-        },
-        'join_date': datetime.date(2023, 1, 15) # (سنستخدمه لاحقاً مع الفلاتر)
-    }
-    
-    # 2. نمرر السياق إلى القالب
-    return render(request, 'profile.html', context) 
+# Create your views here.
