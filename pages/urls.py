b7d60cbcda8db46a8fd02a53e5fa4835e8080fef
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-11-19 17:57:32 +0300

    Erase everything

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ # type: ignore
-    path('', views.index, name='index'),
-    path('/templates', views.templates, name='templates'),
-]
