0d80275f10d57b8361ddbd461fc86ab5bf3bb8b9
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-07 22:09:59 +0300

    بعض التعديلات

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,9 @@ function App() {
         <h1>Vite + React</h1>
         <div className="card"></div>
         </header>
+        <p>
+          test
+        </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
