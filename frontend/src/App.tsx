18b2a68d5401dd9d4b8e85bc2683d137a032ad63
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-10 00:49:01 +0300

    بعض التعديلات

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
 // start my website
 function App() {
   return (
-    <header>
-      <div id="nameLogo">
-      <h1>عوالمنا</h1>
-      <img src="/Website.jpg" alt="Website logo" />
-      </div>
-      <div id="log">
-        <h6>
-          <a href="/login">تسجيل الدخول</a>
-          /
-          <a href="/register">تسجيل حساب جديد</a>
-        </h6>
-      </div>
-    </header>
+    <>
+      <header>
+        <div id="nameLogo">
+          <h1>عوالمنا</h1>
+          <img src="/Website.jpg" alt="Website logo" />
+        </div>
+        <div id="log">
+          {/* Changed h6 to nav or div because h6 is for headers, not links */}
+          <nav>
+            <a href="/login">تسجيل الدخول</a>
+            {' / '}
+            <a href="/register">تسجيل حساب جديد</a>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <p>موقع عالمي لعرض القصص والروايات بكل اللغات مع توفير خدمة ترجمة فورية ودقيقة من وإلى أي لغة.</p>
+      </main>
+    </>
   );
 }
 
