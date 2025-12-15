1e3ab038d6ddc424bbc8aa49f02783406223ef36
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-15 09:47:26 +0300

    Finalizing setup instructions and venv migration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import About from './components/About';
 import './App.css'; // استيراد CSS
+
+const username = "سالم"; // مثال على بيانات
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="page-container" dir="rtl"> {}
-        <Header />
+      <div className="page-container" dir="rtl">
+        <Header userName={username} />
         <Routes>
           <Route path="/" element={<MainContent />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-
         <Footer />
       </div>
     </Router>
@@ -20,3 +23,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
