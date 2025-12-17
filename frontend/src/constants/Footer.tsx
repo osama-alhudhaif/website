6c01d26e4ef9f7af8be475f5f9beb4537cc311fb
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-18 00:03:45 +0300

    Add registration

---
+++ frontend/src/constants/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="center">
+      <footer>
+        <p className="Help">
+          <a href="/contact">تتصل بنا</a>
+        </p>
+        <p className="Rights">
+          &copy; 2023 عوالمنا. جميع الحقوق محفوظة.
+        </p>
+        <p className="About">
+          <a href="/about">من نحن</a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
+
