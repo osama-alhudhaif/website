e54ce7a7637d88730fa48c653cdf980b4bcbb685
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-18 00:03:45 +0300

    Add registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import { Link, Outlet } from 'react-router-dom';
-import './App.css'; 
+import { Outlet } from 'react-router-dom';
+import './App.css';
 import Header from './constants/Header';
+import Footer from './constants/Footer';
 
-function App() {
-  return (
-
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <div className="app-container">
+      <Outlet />
+    </div>
+    <Footer />
+  </>
+);
 
 export default App;
+
