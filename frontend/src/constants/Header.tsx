8777db405e2c7a1ebd67130dc324cff2d780f79f
osama-alhudhaif <osama.alhudhaif@gmail.com>
2025-12-17 22:49:38 +0300

    add Header

---
+++ frontend/src/constants/Header.tsx
@@ -0,0 +1,25 @@
+    <div className="Main-Container">
+      <header className="Header">
+        <div className="Right-Side">
+          <Link to="/" className="Logo-link">
+            <img src="./Website.jpg" alt="Logo" className="logo-img" />
+            <h2 className="Brand-Name">عوالمنا</h2>
+          </Link>
+        </div>
+
+        <div className="Left-Side">
+          <nav className="Nav-Links">
+            <Link to="/login">تسجيل الدخول</Link>
+            <Link to="/register">انشاء حساب</Link>
+          </nav>
+        </div>
+      </header>
+
+      <main className="Content-Area">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // تأكد أنه يستدعي App.tsx الذي كتبنا فيه الـ Router
-import './index.css'
+import { BrowserRouter } from 'react-router-dom' // تأكد من وجود هذا السطر
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
