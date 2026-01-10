84ec321450503129270f5f0942bae81da451c8de
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-01-10 18:22:36 +0300

    add Reading page

---
+++ frontend/src/constants/Reading.tsx
@@ -0,0 +1,10 @@
+const Reading = () => {
+    return (
+        <div className="data" dir="rtl">
+            <h1 style={{ textAlign: "center" }}>عنوان القصة</h1>
+            <h6 style={{ textAlign: "right" }}>المؤلف</h6>
+        </div>
+    )
+}
+
+export default Reading
