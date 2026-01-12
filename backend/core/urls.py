6b7ca9f834cdd243853f025813216598c6537d32
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-01-12 22:17:32 +0300

    تحسين صفحة الادمن

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -11,17 +11,30 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+"""
+URL configuration for core project.
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
+from django.utils.html import format_html
 
-admin.site.site_header = "لوحة التحكم موقع oda"
+# لاحظ: حذفنا المسافات والتعليقات الزائدة في الأعلى لتجنب الخطأ
+# ولاحظ أن المسار أصبح يبدأ بـ /static/ مباشرة
+
+admin.site.site_header = format_html('<img src="/static/Website.png" style="height: 30px; vertical-align: middle;"> لوحة التحكم موقع oda')
 admin.site.site_title = "بوابة الإدارة"
-admin.site.index_title = "لوحة التحكم"
+admin.site.index_title = "مرحباً بك في الإدارة"
+admin.site.enable_nav_sidebar = False
+
+try:
+    admin.site.disable_action('delete_selected')
+except KeyError:
+    pass
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('oda-secret-access/', admin.site.urls, name='admin-secret-access'),
 ]
