eafca70851affaac8158ed0b17d65fdd17d19bf6
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-01-16 00:36:25 +0300

    Add story list

--- frontend/src/constants/Header.tsx
+++ frontend/src/constants/Header.tsx
@@ -6,21 +6,39 @@ const Header = () => {
       <header className="Header">
         <div className="Right-Side">
           <Link to="/" className="Logo-link">
-            <img src="./public/Website.png" alt="Logo" className="logo-img" />
+            {/* تم تصحيح المسار ليعمل بشكل صحيح من مجلد public */}
+            <img src="/Website.png" alt="Logo" className="logo-img" />
             <h2 className="Brand-Name">Oda</h2>
           </Link>
         </div>
 
         <div className="Left-Side">
-          <nav className="Nav-Links">
-            <Link to="/login">تسجيل الدخول</Link>
-            <Link to="/register">انشاء حساب</Link>
-          </nav>
+          {/* أضفنا تنسيق Flexbox لجعل العناصر متراصفة وبمسافات متساوية */}
+<nav className="Nav-Links">
+  {/* حاوية خاصة للقائمة المنسدلة */}
+  <div className="Select-Container">
+    <select className="Custom-Select">
+      <option value="" disabled selected>اختر الفئة</option>
+      <option value="war">حرب</option>
+      <option value="sci-fi">خيال علمي</option>
+      <option value="action">أكشن</option>
+      <option value="fantasy">الفانتازيا</option>
+      <option value="mystery">الجريمة والغموض</option>
+      <option value="horror">الرعب</option>
+      <option value="history">القصص التاريخية</option>
+      <option value="heist">سرقة</option>
+      <option value="adventure">المغامرات</option>
+      <option value="romance">رومانسية</option>
+    </select>
+  </div>
+  
+  <Link to="/login" className="Nav-Item">تسجيل الدخول</Link>
+  <Link to="/register" className="Nav-Item">انشاء حساب</Link>
+</nav>
         </div>
       </header>
-      <div className="Header-Content">
-      </div>
 
+      {/* منطقة المحتوى المتغير */}
       <main className="Content-Area">
         <Outlet />
       </main>
