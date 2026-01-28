6cb6484ffd25864c880475d736441b1ab831bf4d
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-01-28 22:05:40 +0300

    اضافة صفحات مخصصة للحسابات

---
+++ frontend/src/constants/Accounts.tsx
@@ -0,0 +1,10 @@
+const Accounts = () => {
+    return (
+        <div className="container">
+            <h1>Accounts</h1>
+            <p>مرحباً بك في صفحة الحسابات. هنا يمكنك إدارة بياناتك.</p>
+        </div>
+    );
+};
+
+export default Accounts;
---
+++ frontend/src/profel/MyProfile.tsx
@@ -0,0 +1,10 @@
+const MyProfile = () => {
+    return (
+        <div>
+            <h1>Author: Firstname Lastname</h1>
+            <h1>test</h1>
+        </div>
+    );
+};
+
+export default MyProfile;
