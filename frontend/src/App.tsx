a56de04a43cdff7c61db2b75e8392528c0153e69
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-01-28 22:05:40 +0300

    اضافة صفحات مخصصة للحسابات

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,24 +7,26 @@ import Register from './pages/register';
 import About from './constants/about';
 import Body from './constants/body';
 import Reading from './constants/Reading';
+import AuthorProfile from './profel/AuthorProfile';
+import MyProfile from './profel/MyProfile';
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <div className="app-container">
-        <Routes>
-          {/* الصفحة الرئيسية */}
-          <Route path="/" element={<Body />} /> 
-          
-          {/* مسار الفئات الديناميكي */}
-          <Route path="/category/:categoryName" element={<Body />} /> 
-          
-          <Route path="/About" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reading" element={<Reading />} />
-        </Routes>
+<Routes>
+  <Route path="/" element={<Body />} /> 
+  <Route path="/category/:categoryName" element={<Body />} /> 
+  <Route path="/About" element={<About />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/reading" element={<Reading />} />
+
+  {/* أضف هذه الأسطر هنا */}
+  <Route path="/profel/AuthorProfile" element={<AuthorProfile />} />
+  <Route path="/profel/MyProfile" element={<MyProfile />} />
+</Routes>
       </div>
       {/* ملاحظة: نقلت <Body /> ليكون داخل الـ Routes كصفحة رئيسية */}
       <Footer />
---
+++ frontend/src/profel/AuthorProfile.tsx
@@ -0,0 +1,9 @@
+const AuthorProfile = () => {
+    return (
+        <div>
+            <h1>Author: Firstname Lastname</h1>
+        </div>
+    );
+};
+
+export default AuthorProfile;
