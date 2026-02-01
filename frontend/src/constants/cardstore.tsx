3fe3f7bf968b7051a6e077b093c012df482fb669
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-02-01 21:27:45 +0300

    بعض التصليحات

--- frontend/src/constants/cardstore.tsx
+++ frontend/src/constants/cardstore.tsx
@@ -1,9 +1,26 @@
-const Cardstore = () => {
+interface CardstoreProps {
+    name: string
+    description: string
+    likes: number
+}
+
+const Cardstore: FC<CardstoreProps> = ({
+    name = "osama",
+    description = "test",
+    likes = 0,
+}) => {
+    const [nameState, setNameState] = useState(name)
+    const [descriptionState, setDescriptionState] = useState(description)
+    const [likesState, setLikesState] = useState(likes)
+
     return (
         <div>
-            <p></p>
+            <p>{nameState}</p>
+            <p>{descriptionState}</p>
+            <p>{likesState}</p>
         </div>
     )
 }
 
 export default Cardstore
+
