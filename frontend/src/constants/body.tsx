20e90ec4e31e1a2c7668abbc035de813b8b83a53
osama-alhudhaif <osama.alhudhaif@gmail.com>
2026-02-10 20:55:45 +0300

    test thing

--- frontend/src/constants/body.tsx
+++ frontend/src/constants/body.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import Cardstore from "./Cardstore"
 
 const Body = () => {
+    const [nameState] = useState("osama")
+    const [descriptionState] = useState("44")
+    const [likesState] = useState(0)
+
+    const [nameStatesence] = useState("ali")
+    const [descriptionStatesence] = useState("55")
+    const [likesStatesence] = useState(100)
+
     return (
         <div>
-            <p>test</p>
+            <Cardstore name={nameState} description={descriptionState} likes={likesState} />
+            <Cardstore name={nameStatesence} description={descriptionStatesence} likes={likesStatesence} />
         </div>
     )
 }
 
 export default Body
+
